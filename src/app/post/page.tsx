f79ff876afc59184294cb80acf6d485cb8814474
jssatorres <juansataz.dev@gmail.com>
2023-08-19 10:12:49 +0200

    feat: PostPage add style and no-store

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,23 +1,22 @@
-interface Post {
+import { PostCard } from '@/components/postCard/PostCard'
+
+export interface Post {
   id: number;
   title: string;
   body: string;
 }
 
 const fetchPost = () => {
-  return fetch('https://jsonplaceholder.typicode.com/posts')
+  return fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
     .then(res => res.json())
 }
 
 export default async function PostPage () {
   const posts:Post[] = await fetchPost()
   return (
-    <section>
+    <section className='grid grid-cols-[repeat(auto-fill,minmax(19rem,1fr))] gap-14 p-14'>
       {posts.map((post) => (
-        <article key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </article>
+        <PostCard key={post.id} post={post} />
       ))}
 
     </section>
